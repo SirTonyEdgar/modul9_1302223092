0dfb633d68560965ed55fc22e32a321e439333fa
SirTonyEdgar <daffamuhammad625@gmail.com>
2024-05-04 08:59:19 +0700

    tes2

--- modul9_1302223092/Controllers/WeatherForecastController.cs
+++ modul9_1302223092/Controllers/WeatherForecastController.cs
@@ -4,24 +4,24 @@ namespace modul9_1302223092.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class MahasiswaController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<MahasiswaController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public MahasiswaController(ILogger<MahasiswaController> logger)
         {
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet(Name = "GetMahasiswa")]
+        public IEnumerable<Mahasiswa> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index => new Mahasiswa
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
